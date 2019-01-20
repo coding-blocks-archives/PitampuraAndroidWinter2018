c8a34a0555c15effddbf6fa8f5babb3047df83dc
Harshit Dwivedi <harshithdwivedi@gmail.com>
2019-01-20 14:10:18 +0530

    add code for shared prefs
    Signed-off-by: Harshit Dwivedi <harshithdwivedi@gmail.com>

--- Lecture12/SharedPreference/app/src/main/java/com/codingblocks/sharedpreference/MainActivity.kt
+++ Lecture12/SharedPreference/app/src/main/java/com/codingblocks/sharedpreference/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.codingblocks.sharedpreference
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        count = PreferenceManager.getDefaultSharedPreferences(this).getInt("COUNT", 0)
+        tvCount.text = count.toString()
+//        count = getSharedPreferences("test", Context.MODE_PRIVATE).getInt("COUNT",0)
+
+        btnIncrement.setOnClickListener {
+            count++
+            tvCount.text = count.toString()
+        }
     }
+
+    override fun onStop() {
+        super.onStop()
+        //Store the count to a persistent storage
+
+//        val sharedPreferences = getSharedPreferences("test", Context.MODE_PRIVATE)
+
+        val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+        editor.putInt("COUNT", count)
+//        val result = editor.commit()
+        editor.apply()
+    }
+
 }
